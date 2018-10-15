b9c58f7daf4d0aa38d8ca475231b11865e9bacfe
Troels Nielsen <tron@lundbeck.com>
2018-10-15 17:30:26 +0200

    Speed up the generation of menus a bit

--- functions/index.js
+++ functions/index.js
@@ -6,55 +6,41 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+let firestore = admin.firestore()
+function updateMenu() { 
+	let drinks = firestore.collection('drinks').get();
+	let pumps = firestore.collection('pumps').get();
+	return Promise.all([drinks, pumps]).then((res) => {
+	   let allDrinks = res[0];
+	   let allPumps = res[1];
+	   let ingredients = allPumps.docs.map(
+	       pump => pump.get('value')
+	   ).filter(
+	       ingredient => {return ingredient; }
+	   ).reduce((map, obj) => { map[obj] = true; return map; }, {});
+	   let batch = firestore.batch();
+	   allDrinks.forEach(drink => {
+		   let drinkIngredients = drink.get('ingredients');
+		   if (Object.keys(drinkIngredients).some((ingredient) => !ingredients[ingredient])) {
+			   if (drink.get('available')) { 
+				batch.update(drink.ref, {available: false});
+			   }
+		   } else {
+			   if (!drink.get('available')) {
+				batch.update(drink.ref, {available: true});
+			   }
+	           }
+            });
+	    return batch.commit();
+	}).catch(err => console.log(err));
+}
+
 exports.makeMenu = functions.firestore.document('/pumps/{pumpId}')
     .onUpdate((change, context) => {
         var oldIngredient = change.before.data().value;
         var newIngredient = change.after.data().value;
-
-        if (oldIngredient !== newIngredient) {
-            var pumpsRef = change.after.ref.parent;
-            var availableIngredients = [];
-            pumpsRef.get()
-                .then(snapshot => {
-                snapshot.forEach(doc => {
-                    availableIngredients.push(doc.data().value);
-                });
-                return availableIngredients;
-            })
-            .catch(err => {
-                console.log('Error getting documents', err);
-            });
-
-            console.log(availableIngredients);
-
-            var drinksRef = admin.firestore().collection('drinks');
-            drinksRef.get()
-                .then(snapshot => {
-                    const promises = []
-                    snapshot.forEach(doc => {
-                        var drinkIngredients = doc.data().ingredients;
-                        var ing;
-                        var counter = 0;
-                        for (ing in drinkIngredients) {
-                            if (availableIngredients.indexOf(ing.toString()) > -1) {
-                                counter = counter + 1;
-                            }
-                        }
-                        console.log(counter);
-                        console.log(Object.keys(drinkIngredients).length);
-                        if (counter === Object.keys(drinkIngredients).length) {
-                            promises.push(doc.ref.update({available: true }));
-                        }
-                        else {
-                            promises.push(doc.ref.update({available: false }));
-                        }
-                    });
-                    return Promise.all(promises);
-                })
-                .catch(err => {
-                    console.log('Error getting documents', err);
-                });
-
+	if (oldIngredient !== newIngredient) {
+		return updateMenu();
         }
-        // else whatever
-})
+	return Promise.resolve("ok");
+});
